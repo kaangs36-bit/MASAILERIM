120febc0cd2fc7b27df22a9f8bd78cc0a60e21af
kaangs36-bit <kaangs36@gmail.com>
2026-09-15 18:22:52 +0300

    Platform simgelerini otomatik üret

---
+++ scripts/build-icons.mjs
@@ -0,0 +1,8 @@
+import sharp from 'sharp';
+
+await sharp('resources/icon.svg')
+  .resize(1024,1024)
+  .png()
+  .toFile('resources/icon.png');
+
+console.log('Mesailerim uygulama simgesi oluşturuldu.');
