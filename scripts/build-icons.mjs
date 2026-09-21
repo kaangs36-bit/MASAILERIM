2174c5769cbb016e5a29eb036dc7ca46750e429b
kaangs36-bit <kaangs36@gmail.com>
2026-09-21 16:36:44 +0300

    MRL Şantiye Takip: scripts/build-icons.mjs

--- scripts/build-icons.mjs
+++ scripts/build-icons.mjs
@@ -1,8 +1,1 @@
-import sharp from 'sharp';
-
-await sharp('resources/icon.svg')
-  .resize(1024,1024)
-  .png()
-  .toFile('resources/icon.png');
-
-console.log('Mesailerim uygulama simgesi oluşturuldu.');
+import sharp from'sharp';await sharp('resources/icon.svg').resize(1024,1024).png().toFile('resources/icon.png');console.log('Simge hazır.');
